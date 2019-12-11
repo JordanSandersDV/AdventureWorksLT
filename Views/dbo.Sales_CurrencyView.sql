308c9412bbd690669e986a21bf1c761114d6f4a0
JordanSanders <JordanSanders@devart.com>
2019-12-11 14:01:21 +0200

    AW_2017_commit

---
+++ Views/dbo.Sales_CurrencyView.sql
@@ -0,0 +1,14 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE VIEW [dbo].[Sales.CurrencyView] 
+AS SELECT
+  Currency_1.CurrencyCode
+ ,Currency.Name
+ ,Currency.ModifiedDate
+ ,CurrencyRate.CurrencyRateID
+FROM Sales.CurrencyRate
+INNER JOIN Sales.Currency
+  ON CurrencyRate.FromCurrencyCode = Currency.CurrencyCode
+INNER JOIN Sales.Currency Currency_1
+  ON CurrencyRate.FromCurrencyCode = Currency_1.CurrencyCode
+GO
